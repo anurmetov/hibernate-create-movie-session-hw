de13cb84e0129d9b5edec29e6fba6aebd232ad4e
artem <artemnurmetow@gmail.com>
2026-07-07 20:27:42 +0200

    - changed db connection config and added required annotations for injector

--- src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -1,9 +1,11 @@
 package mate.academy.service.impl;
 
 import java.util.List;
+import mate.academy.lib.Service;
 import mate.academy.model.CinemaHall;
 import mate.academy.service.CinemaHallService;
 
+@Service
 public class CinemaHallServiceImpl implements CinemaHallService {
     @Override
     public CinemaHall add(CinemaHall cinemaHall) {
