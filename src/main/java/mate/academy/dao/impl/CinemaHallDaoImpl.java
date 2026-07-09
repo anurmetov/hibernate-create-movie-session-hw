e32a4f107e33257cebd22a67ffcaef2cbc7f98dc
Artem Nurmetov <159072937+anurmetov@users.noreply.github.com>
2026-07-09 21:03:17 +0200

    Update src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
    Co-authored-by: Olena Bruiako <bruyako.elena@gmail.com>

--- src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/CinemaHallDaoImpl.java
@@ -16,6 +16,23 @@ public class CinemaHallDaoImpl implements CinemaHallDao {
     @Override
     public CinemaHall add(CinemaHall cinemaHall) {
         Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(cinemaHall);
+            transaction.commit();
+            return cinemaHall;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert cinema hall " + cinemaHall, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.getTransaction();
             transaction.begin();
